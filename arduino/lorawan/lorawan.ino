0f683151428fb34f6a9b0b4be66c49046a865e85
RyoTozawa <ryo.for.development@gmail.com>
2019-11-26 14:35:46 +0900

    add: date rate change

--- arduino/lorawan/lorawan.ino
+++ arduino/lorawan/lorawan.ino
@@ -11,6 +11,15 @@
 #define TX_PIN  12  // D12: UART_RX 受信用
 #define RX_PIN  11  // D11: UART_TX 送信用
 
+// Data Rate (拡散率＋バンド幅)
+#define SF_12 0 // Long Range / Low Rate
+#define SF_11 1
+#define SF_10 2
+#define SF_9 3
+#define SF_8 4
+#define SF_7 5 // Short Range / High Rate
+
+
 // Instance
 LoRaWAN_TLM922S_SoftwareSerial LoRaWAN(RX_PIN, TX_PIN);
 
@@ -28,17 +37,20 @@ void setup() {
   while (!( LoRaWAN.join(JOIN_OTAA) &&
             LoRaWAN.joinResult() )) delay(1000);
   Serial.println(F("join accepted"));
-  
+
   // TASK: Get DEVEUI
   if (LoRaWAN.getDevEUI() && LoRaWAN.isData()) {
     String deveui = LoRaWAN.getData();
     Serial.println("deveui:" + deveui);
   }
+  // TODO: データレート調整
+  if (LoRaWAN.setDataRate(SF_12)) {
+    Serial.println("lora data rate is %d", SF_12)
+  }
 }
 
 void loop() {
   // TODO: 任意のデータを調整
-  // TODO: 拡散率調整
   // TODO: 送信データサイズを取得 (Sensewayは，GPS（緯度，高度，経度），10バイト(20桁の16進数表記)のデータを送信)
   uint32_t ms = millis();    // or user sensor data
 
