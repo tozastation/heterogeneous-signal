c0c2473f432806bb8752ba2a9649047a93ff9c65
RyoTOZAWA <ryo.for.development@gmail.com>
2019-12-15 23:12:49 +0900

    fix: sequence chart

--- arduino/lorawan/lorawan.ino
+++ arduino/lorawan/lorawan.ino
@@ -28,35 +28,35 @@ void setup() {
   Serial.begin(9600);
   LoRaWAN.begin(9600);
   while (! LoRaWAN.getReady()     );
+  Serial.println("lorawan ready ok");
   while (! LoRaWAN.factoryReset() );
-  while (!( LoRaWAN.join(JOIN_OTAA) &&
-            LoRaWAN.joinResult() ));
-  // --- set up data rate ---
-  if (LoRaWAN.setAdr(ADR_OFF)) {
-    Serial.println(F("adaptive rate off"));
-  }
-  if (LoRaWAN.setDataRate(SF_8)) {
-    Serial.print(F("lora data rate is: 4"));
-  }
-  if (LoRaWAN.loraSave()) {
-    Serial.print(F("save state"));
-  }
+  Serial.println("init lorawan");
+  while (! LoRaWAN.join(JOIN_OTAA));
+  Serial.println("joinning network");
+  while (! LoRaWAN.joinResult());
+  Serial.println("joined network");
+  delay(4000);
 }
 int count = 1;
 void loop() {
+  // send data is current data rate
+  // --- set up data rate ---
+  //if (LoRaWAN.setAdr(ADR_OFF)) {}
+  // --- Change data rate ---
+  //if (LoRaWAN.setDataRate(2)) {}
   uint32_t data = LoRaWAN.getDataRate();
   if ( LoRaWAN.tx(TX_UCNF, 1) &&
-       LoRaWAN.txData(data)     &&
+       LoRaWAN.txData(count)     &&
        LoRaWAN.txRequest()    &&
        LoRaWAN.txResult())
   {
     Serial.println("count: " + String(count));
     Serial.println("send data: " + String(data));
-    // --- enter sleep in 5 minutes ---
+    // --- enter sleep in 5 seconds ---
     pinMode(7, OUTPUT);
     digitalWrite(7, LOW);
     if (LoRaWAN.sleep(0)) {
-      delay(5000);
+      delay(4000);
       digitalWrite(7, HIGH);
     }
     count++;
